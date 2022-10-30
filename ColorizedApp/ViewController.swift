216be313504f414c1001becd5f7df05ada1614a3
Margarita Konovalevskaya <sov-team@ya.ru>
2022-10-30 23:34:44 +0700

    first commit

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// MARK: - IB Outlets
+   
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 15
+        
     }
 
+// MARK: - IB Actions
+   
+    @IBAction func sliderAction(_ sender: UISlider) {
+        
+        switch sender.tag {
+        case 0: redLabel.text = String(format: "%.2f", sender.value)
+        case 1: greenLabel.text = String(format: "%.2f", sender.value)
+        default: blueLabel.text = String(format: "%.2f", sender.value)
+        }
+        
+        setColor()
+    }
+    
+// MARK: - Private Methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
 
 }
 
